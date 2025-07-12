82129fbeccb05f32bc36f9617842d631f87b2d0d
chaitanyakumar <gchkumar999@gmail.com>
2025-07-12 15:32:58 +0530

    ok

---
+++ Typescript_mui/src/App.tsx
@@ -0,0 +1,49 @@
+// import { useState } from "react";
+
+// function Counter() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div>
+//       <h1>you clicked {count}</h1>
+//       <button onClick={()=>setCount (count +1)}>Click Here</button>
+//     </div>
+//   )
+
+// }
+
+// export default Counter
+
+
+// let  Hero: string
+
+// function getHero() {
+//     return "Hello"
+// }
+
+// Hero = getHero()
+
+
+// export default App
+
+
+
+// let Hero: string;
+
+// function getHero() {
+//   return "Hello";
+// }
+
+// Hero = getHero();
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>{Hero}</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+let Hero :String;
